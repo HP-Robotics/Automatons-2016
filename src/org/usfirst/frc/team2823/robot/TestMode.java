2306840c34df33d95e7245dc63276358aa4045fb
Highland Park 2823 <teamautomatons2823@gmail.com>
2016-02-27 14:52:52 -0600

    Testing Motion Profiling
    Minor changes for testing motion profiling.

--- src/org/usfirst/frc/team2823/robot/TestMode.java
+++ src/org/usfirst/frc/team2823/robot/TestMode.java
@@ -16,18 +16,29 @@ public class TestMode {
 		robot.lDriveEncoder.reset();
 		robot.rDriveEncoder.reset();
 		
-		robot.motionDriveControl.configureGoal(50, Robot.MAXVELOCITY, Robot.MAXACCELERATION);
 		//robot.armControl.setSetpoint(Robot.HIGHTRAVELSETPOINT);
 		//robot.armControl.enable();
 		
-		//robot.gyroDriveControl.enableLog("TestGyroPID.csv");
-		//robot.gyroDriveControl.enable();
+		/*robot.gyroDriveControl.enableLog("TestGyroPID.csv");
 		
 		//robot.gyroDriveControl.setSetpoint(SmartDashboard.getNumber("TestGyro Target (Inches)"));
+		robot.gyroDriveControl.setOutputRange(-0.5,  0.5);
+		robot.gyroDriveControl.setSetpoint(1000);
+		
+		robot.gyroDriveControl.enable();*/
+		
+		//robot.driveRobot(0.5, 0.5);
+		initTime = Timer.getFPGATimestamp();
 	}
 	
 	public void testPeriodic() {
 		robot.motionDriveControl.setPID(SmartDashboard.getNumber("P"), SmartDashboard.getNumber("I"), SmartDashboard.getNumber("D"));
+		//robot.gyroDriveControl.setPID(SmartDashboard.getNumber("P"), SmartDashboard.getNumber("I"), SmartDashboard.getNumber("D"));
+		
+		/*if(Timer.getFPGATimestamp() - initTime > 4) {
+			robot.gyroDriveControl.disable();
+			robot.gyroDriveControl.closeLog();
+		}*/
 		
     	if(robot.stick.getRawButton(Robot.ABUTTON)) {
     		if (!robot.motionDriveEnabled) {
@@ -36,7 +47,7 @@ public class TestMode {
     			robot.slowDriveEnabled = false;
 
     			robot.motionDriveControl.enableLog("motionControlPID.csv");
-    			robot.motionDriveControl.configureGoal(50, Robot.MAXVELOCITY, Robot.MAXACCELERATION);
+    			robot.motionDriveControl.configureGoal(50, Robot.MAXVELOCITY/3, Robot.MAXACCELERATION/5);
     			robot.motionDriveControl.enable();
     		}
     		
